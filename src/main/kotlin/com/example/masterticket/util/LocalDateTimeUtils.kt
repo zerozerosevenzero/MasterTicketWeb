3e8d0d55c958b2dd18e7504cbf510ec159252995
KingBlackCow <sgs1159@naver.com>
2023-05-31 00:48:45 +0900

    refactor: 안쓰는 코드 제거

--- src/main/kotlin/com/example/masterticket/util/LocalDateTimeUtils.kt
+++ src/main/kotlin/com/example/masterticket/util/LocalDateTimeUtils.kt
@@ -3,14 +3,11 @@ package com.example.masterticket.util
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.time.temporal.WeekFields
-import java.util.*
 
 class LocalDateTimeUtils {
     companion object {
         val YYYY_MM_DD_HH_MM = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
         val YYYY_MM_DD = DateTimeFormatter.ofPattern("yyyyMMdd")
-        val YYYY_MM_DD2 = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val MM_DD = DateTimeFormatter.ofPattern("MM-dd")
 
         fun format(localDateTime: LocalDateTime): String {
@@ -26,10 +23,6 @@ class LocalDateTimeUtils {
             else {
                 LocalDate.parse(localDateTimeString, YYYY_MM_DD).atStartOfDay()
             }
-        }
-
-        fun getWeekOfYear(localDateTime: LocalDateTime): Int {
-            return localDateTime[WeekFields.of(Locale.KOREA).weekOfYear()]
         }
 
         fun parseDate(localDateTimeString: String): LocalDateTime? {
